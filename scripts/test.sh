d9b87580d38f8e0a3d8cd7e3586519ceaec95106
Bill Lovett <bill@ilovett.com>
2026-07-06 15:56:19 -0400

    Check for presence of prove6 during test script.

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,10 @@ set -eu
 
 cd "$(dirname "$0")/../"
 
+if [ ! -f "$HOME/.raku/bin/prove6" ]; then
+    zef install App::Prove6
+fi
+
 echo "Running prove6 in quiet mode"
 
 prove6 -q --jobs 8  --lib t/ xt/
